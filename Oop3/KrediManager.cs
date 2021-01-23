3164f11bacd15240335e33cab8b361fc83c10fd8
Mert KUL <kul.mertt@gmail.com>
2021-01-24 00:03:06 +0300

    24.01.2021

---
+++ Oop3/KrediManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop3
+{
+    //operasyon
+    interface IKrediManager
+    {
+        void Hesapla();
+        void BiseyYap();
+
+    }
+}
---
+++ Oop3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Oop3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            
+
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            
+
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(new EsnafKrediManager(),fileLoggerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {konutKrediManager,ihtiyacKrediManager,new EsnafKrediManager() };
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+
+        }
+    }
+}
